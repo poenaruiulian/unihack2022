fb6ad4213db4bad902281629f4bb5d7557448c2b
poenaruiulian <poenaruiulian2003@gmail.com>
2022-11-26 00:57:35 +0200

    Login&Signup forms ready

--- db.py
+++ db.py
@@ -6,4 +6,4 @@ def db_init(app):
     db.init_app(app)
 
     with app.app_context():
-        db.create_all()
+        db.create_all() 
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask,session,request,redirect,url_for,render_template
+from flask import Flask,flash,session,request,redirect,url_for,render_template
 from datetime import timedelta
+import bcrypt
 
 from db import db, db_init
 from models import Users
@@ -13,27 +14,92 @@ db_init(app)
 
 userInSession = False
 
-@app.route("/", methods=['GET','POST'])
-def home():
+@app.route("/login", methods=['GET','POST'])
+def login():
+
+    message = None
+
     if userInSession == True:
         return redirect(url_for("dashboard"))
     else: 
-        return render_template("login.html")
+
+        if request.method=='POST':
+            mailLogin = request.form["e-mail"]
+            passLogin = request.form["password"]
+
+            existUser = Users.query.filter_by(mail = mailLogin).first()
+            if not existUser: message = "Try making an account!"
+            else:
+                if bcrypt.checkpw(passLogin.encode('utf-8'), existUser.password):
+                    session["userInSession"] = mailLogin
+                    message = "Now you are signed in!"
+                    session.permanent = True
+                    return redirect(url_for("dashboard"))
+                else: message="Password incorect!"
+        
+    if message != None:
+        flash(message)
+    return render_template("login.html")
 
 @app.route("/register", methods = ['POST', 'GET'])
 def register():
+
+    message = None
+
     if request.method == 'POST':
 
+        
         session.permanent = True
 
-        firstNameRegister = request.form["first_name"]
-        lastNameRegister = request.form["last_name"]
-        passRegister = request.form["passReg"]
-        passRegisterVerif = request.form["passRegVerif"]
-        mailRegister = request.form["mail"]
+        firstNameRegister = request.form["firstname"]
+        lastNameRegister = request.form["lastname"]
+        passRegister = request.form["password"]
+        passRegisterVerif = request.form["cpassword"]
+        mailRegister = request.form["e-mail"]
+
+    
+
+        existUser = Users.query.filter_by(mail = mailRegister).first()
+        
+        if passRegister==passRegisterVerif:
+            if existUser: message = "This email allready exist!"
+            else:
+                hashed = bcrypt.hashpw(passRegister.encode('utf-8'), bcrypt.gensalt())
+
+                addUser = Users(
+                    first_name = firstNameRegister,
+                    last_name = lastNameRegister,
+                    password = hashed,
+                    mail = mailRegister
+                )
+
+                db.session.add(addUser)
+                db.session.commit()
+
+                session['userInSession'] = mailRegister
+                message = 'Welcome'
+        else:
+            message = "The passwords don't match,Try again."
+
+    if message != None:
+        flash(message)
+    return render_template("register.html")
 
 
 
+@app.route("/dashboard", methods=['GET','POST'])
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/signout")
+def signout():
+    if "userInSession" in session:
+        session.pop("userInSession", None)
+        return redirect(url_for("login"))
+    else:
+        return redirect(url_for("login"))
 
 if __name__== "__main__":
+
     app.run(debug=True)
